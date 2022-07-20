e3aaa8cadf8ff8df63027e70fd9f1f38c2f3dca7
Harun Nurahman <harun.nurrahman@hotmail.com>
2022-07-20 23:13:37 +0700

    Slicing UI Sign In Page

--- lib/pages/signin_page.dart
+++ lib/pages/signin_page.dart
@@ -6,36 +6,119 @@ class SignInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final bottomInset = MediaQuery.of(context).viewInsets.bottom;
+
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 24, top: 30),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Title
-              Text(
-                'Sign In',
-                style: grayTextStyle,
-              ),
-              const SizedBox(height: 2),
-              // Subtitle
-              Text(
-                'Build Your Career',
-                style: blackTextStyle.copyWith(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w600,
+        child: SingleChildScrollView(
+          reverse: true,
+          child: Padding(
+            padding: EdgeInsets.only(
+              left: 24,
+              right: 24,
+              top: 30,
+              bottom: bottomInset,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Title
+                Text(
+                  'Sign In',
+                  style: grayTextStyle,
                 ),
-              ),
-              const SizedBox(height: 40),
-              // Illustration
-              Center(
-                child: Image.asset(
-                  'assets/images/img_explore-jobs.png',
-                  height: 240,
+                const SizedBox(height: 2),
+                // Subtitle
+                Text(
+                  'Build Your Career',
+                  style: blackTextStyle.copyWith(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-              )
-            ],
+                const SizedBox(height: 40),
+                // Illustration
+                Center(
+                  child: Image.asset(
+                    'assets/images/img_explore-jobs.png',
+                    height: 240,
+                  ),
+                ),
+                const SizedBox(height: 40),
+                // Text Field
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Email Text Field
+                    Text(
+                      'Email Address',
+                      style: grayTextStyle,
+                    ),
+                    const SizedBox(height: 8),
+                    SizedBox(
+                      height: 45,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          fillColor: grayColor_2,
+                          filled: true,
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide.none,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    // Password Text Field
+                    Text(
+                      'Password',
+                      style: grayTextStyle,
+                    ),
+                    const SizedBox(height: 8),
+                    SizedBox(
+                      height: 45,
+                      child: TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          fillColor: grayColor_2,
+                          filled: true,
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide.none,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 40),
+                    // Sign In Button
+                    Container(
+                      width: MediaQuery.of(context).size.width * 1,
+                      height: 45,
+                      child: TextButton(
+                        onPressed: () {},
+                        style: TextButton.styleFrom(
+                          backgroundColor: primaryColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        child: Text('Sign In', style: whiteTextStyle),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/shared.dart
+++ lib/shared.dart
@@ -7,6 +7,7 @@ Color secondaryColor = const Color(0xFF2A327D);
 Color redColor = const Color(0xFFFD4F56);
 Color blackColor = const Color(0xFF272C2F);
 Color grayColor = const Color(0xFFB3B5C4);
+Color grayColor_2 = const Color(0xFFF1F0F5);
 
 TextStyle grayTextStyle = GoogleFonts.poppins(
   fontSize: 16,
@@ -15,6 +16,12 @@ TextStyle grayTextStyle = GoogleFonts.poppins(
 
 TextStyle blackTextStyle = GoogleFonts.poppins(
   fontSize: 16,
-  color: blackColor,
   fontWeight: FontWeight.w500,
+  color: blackColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  color: Colors.white,
 );
