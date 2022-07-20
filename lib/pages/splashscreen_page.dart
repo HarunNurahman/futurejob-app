9432685aa5997c1756e52b1e6f143160420dd4dd
Harun Nurahman <harun.nurrahman@hotmail.com>
2022-07-20 14:04:36 +0700

    Creating Splash Screen

--- lib/pages/splashscreen_page.dart
+++ lib/pages/splashscreen_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:futurejob_app/shared.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -11,7 +13,24 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: primaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/icons/ic_logo.png', height: 76),
+            const SizedBox(height: 50),
+            Text(
+              'FUTUREJOB',
+              style: GoogleFonts.poppins(
+                fontSize: 32,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/shared.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = const Color(0xFF4141A4);
+Color secondaryColor = const Color(0xFF2A327D);
+
+Color redColor = const Color(0xFFFD4F56);
+Color blackColor = const Color(0xFF272C2F);
+Color grayColor = const Color(0xFFB3B5C4);
